40969dc7932a9e4e1a83206ca948485d7cf2c912
Neil McCaffrey <possiblyneil@gmail.com>
2021-04-04 19:43:48 +0100

    cleaning up my local repo

--- keyboards/handwired/janck/keymaps/default/keymap.c
+++ keyboards/handwired/janck/keymaps/default/keymap.c
@@ -1,18 +1,38 @@
 #include "janck.h"
+#include QMK_KEYBOARD_H
+
+enum {
+    PASTE,
+    FFSC,
+    WINSC,
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+    [PASTE] = ACTION_TAP_DANCE_DOUBLE(KC_Q, LSFT(KC_INS)),
+    [FFSC]  = ACTION_TAP_DANCE_DOUBLE(KC_A, RCS(KC_S)),
+    [WINSC] = ACTION_TAP_DANCE_DOUBLE(KC_ESC, SGUI(KC_S)),
+};
+
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    // this is some garbage to test the rgb lights
+/*
 	KEYMAP(
 		RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
 		RGB_RMOD, RGB_HUD, RGB_SAD, RGB_VAD, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
 		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, 
 		BL_TOGG, KC_LCTL, KC_LALT, KC_LGUI, MO(1), KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
-/*
-	KEYMAP(
-		KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC, 
-		KC_ESC, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, 
-		KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_ENT, 
-		BL_TOGG, KC_LCTL, KC_LALT, KC_LGUI, MO(1), KC_SPC, MO(2), KC_LEFT, KC_DOWN, KC_UP, KC_RGHT),
 */
+	KEYMAP(
+//,-----------------------------------------------------------------------------------------------------------.
+     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_BSPC, 
+//|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+     KC_ESC,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT, 
+//|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_ENT, 
+//|--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------+--------|
+    BL_TOGG, KC_LCTL, KC_LALT, KC_LGUI,   MO(1),  KC_SPC,   MO(2), KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_NO),
+//,-----------------------------------------------------------------------------------------------------------.
 
 	KEYMAP(
 		KC_TILD, KC_EXLM, KC_AT, KC_HASH, KC_DLR, KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TRNS, 
